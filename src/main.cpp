31025780740d0ca7845d5fd003bf1f97b4675ce3
Marko-Sanchez <sanchezmarco0909@gmail.com>
2025-06-11 00:25:45 -0700

    Creating the window

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,44 @@
+#include <GL/gl.h>
+#include <cstdlib>
 #include <iostream>
+#include <memory>
+
+#include <GLFW/glfw3.h>
 
 int main()
 {
     std::cout << "Hello World" << std::endl;
-    return 0;
+
+    if (!glfwInit())
+    {
+        std::cerr << "Failed to Initialize GLFW" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    std::shared_ptr<GLFWwindow> window(glfwCreateWindow(1080, 1080, "GAME 9", nullptr, nullptr), glfwDestroyWindow);
+    if (!window.get())
+    {
+        std::cerr << "Window failed to Create" << std::endl;
+
+        glfwTerminate();
+        return EXIT_FAILURE;
+    }
+
+    // Make Window Current Context, set swap interval to wait for 1 screen update before swapping buffers.
+    glfwMakeContextCurrent(window.get());
+    glfwSwapInterval(1);
+
+    while (!glfwWindowShouldClose(window.get()))
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+        // Swap Front buffer (currently being displayed) and back buffer (next to render).
+        glfwSwapBuffers(window.get());
+
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return EXIT_SUCCESS;
 }
