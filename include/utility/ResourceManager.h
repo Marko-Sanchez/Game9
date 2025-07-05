377ead8094d7bcc3a8aa21bee31d2d8ebc94ff7b
Marko-Sanchez <sanchezmarco0909@gmail.com>
2025-07-04 22:05:25 -0700

    Something about using lvalues in data structure, was causing issues with class lifetime. Putting the values in shared_ptr, solved this.

--- include/utility/ResourceManager.h
+++ include/utility/ResourceManager.h
@@ -4,6 +4,7 @@
 #include "utility/Shader.h"
 #include "utility/Texture2D.h"
 
+#include <memory>
 #include <functional>
 #include <optional>
 #include <string>
@@ -11,15 +12,19 @@
 
 class ResourceManager
 {
+private:
+    using shared_shader = std::shared_ptr<Shader>;
+    using shared_texture = std::shared_ptr<Texture2D>;
+
 public:
-    static std::map<std::string, Shader, std::less<>> m_shader;
-    static std::map<std::string, Texture2D, std::less<>> m_textures;
+    static std::map<std::string, shared_shader, std::less<>> m_shader;
+    static std::map<std::string, shared_texture, std::less<>> m_textures;
 
-    static std::optional<Shader> LoadShader(const std::string_view vertexPath, const std::string_view fragmentPath, const std::string_view shaderName);
-    static std::optional<Shader> GetShader(const std::string_view shaderName);
+    static std::optional<shared_shader> LoadShader(const std::string_view vertexPath, const std::string_view fragmentPath, const std::string_view shaderName);
+    static std::optional<shared_shader> GetShader(const std::string_view shaderName);
 
-    static std::optional<Texture2D> LoadTexture(const std::string_view texturePath, const std::string_view textureName);
-    static std::optional<Texture2D> GetTexture(const std::string_view textureName);
+    static std::optional<shared_texture> LoadTexture(const std::string_view texturePath, const std::string_view textureName);
+    static std::optional<shared_texture> GetTexture(const std::string_view textureName);
 
     // TODO: function to remove un-used textures and shaders.
 private:
--- src/utility/ResourceManager.cpp
+++ src/utility/ResourceManager.cpp
@@ -6,10 +6,10 @@
 #include <system_error>
 
 // Instatiate static variables
-std::map<std::string, Shader, std::less<>> ResourceManager::m_shader;
-std::map<std::string, Texture2D, std::less<>> ResourceManager::m_textures;
+std::map<std::string, ResourceManager::shared_shader, std::less<>> ResourceManager::m_shader;
+std::map<std::string, ResourceManager::shared_texture, std::less<>> ResourceManager::m_textures;
 
-std::optional<Shader> ResourceManager::LoadShader(const std::string_view vertexPath, const std::string_view fragmentPath, const std::string_view shaderName)
+std::optional<ResourceManager::shared_shader> ResourceManager::LoadShader(const std::string_view vertexPath, const std::string_view fragmentPath, const std::string_view shaderName)
 {
     std::error_code ec;
     if (!std::filesystem::exists(vertexPath, ec) || !std::filesystem::exists(fragmentPath, ec))
@@ -21,8 +21,9 @@ std::optional<Shader> ResourceManager::LoadShader(const std::string_view vertexP
     if (auto iter = m_shader.find(shaderName); iter == m_shader.end())
     {
         std::string name{shaderName};
-        m_shader[name] = Shader();
-        m_shader[name].LoadShader(vertexPath, fragmentPath);
+        auto shader = std::make_shared<Shader>();
+        shader->LoadShader(vertexPath, fragmentPath);
+        m_shader[name] = shader;
 
         return m_shader[name];
     }
@@ -32,7 +33,7 @@ std::optional<Shader> ResourceManager::LoadShader(const std::string_view vertexP
 }
 
 // TODO: figure out pathing.
-std::optional<Texture2D> ResourceManager::LoadTexture(const std::string_view texturePath, const std::string_view textureName)
+std::optional<ResourceManager::shared_texture> ResourceManager::LoadTexture(const std::string_view texturePath, const std::string_view textureName)
 {
     std::error_code ec;
     if (!std::filesystem::exists(texturePath, ec))
@@ -44,8 +45,9 @@ std::optional<Texture2D> ResourceManager::LoadTexture(const std::string_view tex
     if (auto iter = m_textures.find(textureName); iter == m_textures.end())
     {
         std::string name{textureName};
-        m_textures[name] = Texture2D();
-        m_textures[name].GenerateTexture(texturePath);
+        auto texture = std::make_shared<Texture2D>();
+        texture->GenerateTexture(texturePath);
+        m_textures[name] = texture;
 
         return m_textures[name];
     }
@@ -54,7 +56,7 @@ std::optional<Texture2D> ResourceManager::LoadTexture(const std::string_view tex
     return {};
 }
 
-std::optional<Shader> ResourceManager::GetShader(const std::string_view shaderName)
+std::optional<ResourceManager::shared_shader> ResourceManager::GetShader(const std::string_view shaderName)
 {
     if (auto iter = m_shader.find(shaderName); iter != m_shader.end())
     {
@@ -64,7 +66,7 @@ std::optional<Shader> ResourceManager::GetShader(const std::string_view shaderNa
     return {};
 }
 
-std::optional<Texture2D> ResourceManager::GetTexture(const std::string_view textureName)
+std::optional<ResourceManager::shared_texture> ResourceManager::GetTexture(const std::string_view textureName)
 {
     if (auto iter = m_textures.find(textureName); iter != m_textures.end())
     {
