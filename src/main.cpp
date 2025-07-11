ba4dda1ab6d18b44b29bb23f53118545ce7c4d16
Marko-Sanchez <sanchezmarco0909@gmail.com>
2025-07-10 23:14:08 -0700

    Changed from using Window class to Game class to render within loop.

--- src/main.cpp
+++ src/main.cpp
@@ -4,16 +4,18 @@
 #include <GLFW/glfw3.h>
 
 #include <cstdlib>
+#include <filesystem>
 #include <iostream>
 #include <memory>
 
-#include "ui/window.h"
+#include "scene/Game.h"
+
 int main()
 {
     using namespace Game9;
 
-    constexpr int WINDOW_WIDTH{1080};
-    constexpr int WINDOW_HEIGHT{1080};
+    constexpr int WINDOW_WIDTH{1024};
+    constexpr int WINDOW_HEIGHT{1024};
 
     if (!glfwInit())
     {
@@ -30,6 +32,10 @@ int main()
         return EXIT_FAILURE;
     }
 
+    // TODO: setup icon here:?
+    //
+    //
+
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
@@ -38,6 +44,11 @@ int main()
     glfwMakeContextCurrent(glfw_window.get());
     glfwSwapInterval(1);
 
+    // OpenGL configuration.
+    glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
     if (GLenum err = glewInit(); err != GLEW_OK)
     {
         std::cerr << "Failed to Initialize GLEW\nNeeds a valid OpenGL Context\n";
@@ -45,17 +56,35 @@ int main()
         return EXIT_FAILURE;
     }
 
-    Window window(glfw_window, WINDOW_WIDTH, WINDOW_HEIGHT);
+    Game game9(glfw_window, WINDOW_WIDTH, WINDOW_HEIGHT);
+
+    // Initialize game (load shaders/textures).
+    game9.Init();
+
+    float lastFrame{};
+    float deltaTime{};
+
     while (!glfwWindowShouldClose(glfw_window.get()))
     {
-        glClear(GL_COLOR_BUFFER_BIT);
-        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        // Calculate delta time and processes callbacks.
+        float currentFrame{static_cast<float>(glfwGetTime())};
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+        glfwPollEvents();
 
-        // window.render();
+        // manage user input.
+        game9.ProcessInput(deltaTime);
+
+        // TODO: update game state.
+        // game9.Update();
+
+        // Render.
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        game9.Render();
 
         // Swap Front buffer (currently being displayed) and back buffer (next to render).
         glfwSwapBuffers(glfw_window.get());
-        glfwPollEvents();
     }
 
     glfwTerminate();
