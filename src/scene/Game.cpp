83338e0b6d08e257c6259744c98324e4debf86d3
Marko-Sanchez <sanchezmarco0909@gmail.com>
2025-07-19 00:41:38 -0700

    Milestone: Background is Set, was confused with glm::ortho() but noticed my near and far planes where set incorrectly. I also had the model scale value set incorrectly, when working with window width-height. Was using a scale value:20.0f for (-4, 4) instead of scale value: 1280for (0, 1280)

--- src/scene/Game.cpp
+++ src/scene/Game.cpp
@@ -2,10 +2,11 @@
 
 #include "glm/ext/matrix_clip_space.hpp"
 
-#include "ui/window.h"
+#include <GL/gl.h>
+
 #include "utility/ResourceManager.h"
 
-SpriteRenderer *Renderer;
+std::unique_ptr<SpriteRenderer> Renderer;
 
 Game::Game(std::shared_ptr<GLFWwindow> window, int width, int height):
 m_state(GAME_ACTIVE),
@@ -14,30 +15,32 @@ m_window(window, width, height)
 
 Game::~Game()
 {
-    delete Renderer;
+    Renderer.reset();
 }
 
 void Game::Init()
 {
     // Load shaders.
-    ResourceManager::LoadShader("resources/Shaders/background.vertex", "resources/Shaders/background.fragment", "background");
+    ResourceManager::LoadShader("resources/shaders/background.vertex", "resources/shaders/background.fragment", "background");
 
-    // Configure shaders.
-    glm::mat4 projection = glm::ortho(0.0f, static_cast<float>(m_window.GetWidth()), static_cast<float>(m_window.GetHeight()), 0.0f, 1.0f, 1.0f);
-    ResourceManager::GetShader("background")->Bind();
-    ResourceManager::GetShader("background")->SetUniform1i("u_image", 0);
-    ResourceManager::GetShader("background")->SetUniformMat4f("u_projection", projection);
+    // Configure shaders:
+    // bottom-left corner is (0,0) top-right is (width, height).
+    glm::mat4 projection = glm::ortho(0.0f, static_cast<float>(m_window.GetWidth()), 0.0f, static_cast<float>(m_window.GetHeight()), -1.0f, 1.0f);
+
+    ResourceManager::GetShader("background").value()->Bind();
+    ResourceManager::GetShader("background").value()->SetUniform1i("u_image", 0);
+    ResourceManager::GetShader("background").value()->SetUniformMat4f("u_projection", projection);
 
     // TODO: temp for testing. (static-renderer, etc.)
-    Renderer = new SpriteRenderer(ResourceManager::GetShader("background").value());
+    Renderer = std::make_unique<SpriteRenderer>(ResourceManager::GetShader("background").value());
 
     // Load Textures.
-    ResourceManager::LoadTexture("resources/images/background.png", "map");
+    ResourceManager::LoadTexture("resources/images/background.png", "background");
 }
 
 void Game::Render()
 {
-    Renderer->DrawSprite(ResourceManager::GetTexture("map").value());
+    Renderer->DrawSprite(ResourceManager::GetTexture("background").value(), glm::vec2(0.0f, 0.0f), glm::vec2(static_cast<float>(m_window.GetWidth()), static_cast<float>(m_window.GetHeight())));
 }
 
 void Game::ProcessInput(float deltaTime)
