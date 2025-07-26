f424f9957980051be393ca6f92813e05dc3bfafb
Marko-Sanchez <sanchezmarco0909@gmail.com>
2025-07-25 22:23:36 -0700

    First entity present.

--- src/scene/Game.cpp
+++ src/scene/Game.cpp
@@ -22,6 +22,8 @@ void Game::Init()
 {
     // Load shaders.
     ResourceManager::LoadShader("resources/shaders/background.vertex", "resources/shaders/background.fragment", "background");
+    // TODO: change shader to use same glCreateShader ID for different programs that use the same, shader.
+    ResourceManager::LoadShader("resources/shaders/background.vertex", "resources/shaders/background.fragment", "train");
 
     // Configure shaders:
     // bottom-left corner is (0,0) top-right is (width, height).
@@ -31,16 +33,24 @@ void Game::Init()
     ResourceManager::GetShader("background").value()->SetUniform1i("u_image", 0);
     ResourceManager::GetShader("background").value()->SetUniformMat4f("u_projection", projection);
 
+    ResourceManager::GetShader("train").value()->Bind();
+    ResourceManager::GetShader("train").value()->SetUniform1i("u_image", 0);
+    ResourceManager::GetShader("train").value()->SetUniformMat4f("u_projection", projection);
+
     // TODO: temp for testing. (static-renderer, etc.)
     Renderer = std::make_unique<SpriteRenderer>(ResourceManager::GetShader("background").value());
 
     // Load Textures.
     ResourceManager::LoadTexture("resources/images/background.png", "background");
+    ResourceManager::LoadTexture("resources/images/train.png", "train");
 }
 
 void Game::Render()
 {
     Renderer->DrawSprite(ResourceManager::GetTexture("background").value(), glm::vec2(0.0f, 0.0f), glm::vec2(static_cast<float>(m_window.GetWidth()), static_cast<float>(m_window.GetHeight())));
+    Renderer->DrawSprite(ResourceManager::GetTexture("train").value(),
+                         glm::vec2(static_cast<float>(m_window.GetWidth()) / 2, static_cast<float>(m_window.GetHeight()) / 2),
+                         glm::vec2(256, 256));
 }
 
 void Game::ProcessInput(float deltaTime)
