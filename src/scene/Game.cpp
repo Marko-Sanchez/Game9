506a03936a8f8ba19fcddb8dfecc5c9edd8dd9aa
Marko-Sanchez <sanchezmarco0909@gmail.com>
2025-07-26 22:23:26 -0700

    Added new Renderer object for entitys, this will be developed further in a future branch to handle entity movement.

--- src/scene/Game.cpp
+++ src/scene/Game.cpp
@@ -6,7 +6,10 @@
 
 #include "utility/ResourceManager.h"
 
-std::unique_ptr<SpriteRenderer> Renderer;
+// Issue uses the same shader for background and train but still works.
+// Multiple SpriteRenderers for different things like background and entitys ?
+std::unique_ptr<SpriteRenderer> sceneRenderer;
+std::unique_ptr<SpriteRenderer> entityRenderer;
 
 Game::Game(std::shared_ptr<GLFWwindow> window, int width, int height):
 m_state(GAME_ACTIVE),
@@ -15,14 +18,17 @@ m_window(window, width, height)
 
 Game::~Game()
 {
-    Renderer.reset();
+    sceneRenderer.reset();
+    entityRenderer.reset();
 }
 
+/*
+* Loads Shaders and Textures, sets the intial values for shader uniforms.
+*/
 void Game::Init()
 {
     // Load shaders.
     ResourceManager::LoadShader("resources/shaders/background.vertex", "resources/shaders/background.fragment", "background");
-    // TODO: change shader to use same glCreateShader ID for different programs that use the same, shader.
     ResourceManager::LoadShader("resources/shaders/background.vertex", "resources/shaders/background.fragment", "train");
 
     // Configure shaders:
@@ -34,21 +40,22 @@ void Game::Init()
     ResourceManager::GetShader("background").value()->SetUniformMat4f("u_projection", projection);
 
     ResourceManager::GetShader("train").value()->Bind();
-    ResourceManager::GetShader("train").value()->SetUniform1i("u_image", 0);
+    ResourceManager::GetShader("train").value()->SetUniform1i("u_image", 1);
     ResourceManager::GetShader("train").value()->SetUniformMat4f("u_projection", projection);
 
     // TODO: temp for testing. (static-renderer, etc.)
-    Renderer = std::make_unique<SpriteRenderer>(ResourceManager::GetShader("background").value());
+    sceneRenderer = std::make_unique<SpriteRenderer>(ResourceManager::GetShader("background").value());
+    entityRenderer = std::make_unique<SpriteRenderer>(ResourceManager::GetShader("train").value());
 
     // Load Textures.
-    ResourceManager::LoadTexture("resources/images/background.png", "background");
-    ResourceManager::LoadTexture("resources/images/train.png", "train");
+    ResourceManager::LoadTexture("resources/images/background.png", "background", 0);
+    ResourceManager::LoadTexture("resources/images/train.png", "train", 1);
 }
 
 void Game::Render()
 {
-    Renderer->DrawSprite(ResourceManager::GetTexture("background").value(), glm::vec2(0.0f, 0.0f), glm::vec2(static_cast<float>(m_window.GetWidth()), static_cast<float>(m_window.GetHeight())));
-    Renderer->DrawSprite(ResourceManager::GetTexture("train").value(),
+    sceneRenderer->DrawSprite(ResourceManager::GetTexture("background").value(), glm::vec2(0.0f, 0.0f), glm::vec2(static_cast<float>(m_window.GetWidth()), static_cast<float>(m_window.GetHeight())));
+    entityRenderer->DrawSprite(ResourceManager::GetTexture("train").value(),
                          glm::vec2(static_cast<float>(m_window.GetWidth()) / 2, static_cast<float>(m_window.GetHeight()) / 2),
                          glm::vec2(256, 256));
 }
