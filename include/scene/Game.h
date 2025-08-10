7ddcd49676da263e169da77d0cf20a0c77a9e48a
Marko-Sanchez <sanchezmarco0909@gmail.com>
2025-08-09 22:17:13 -0700

    DataStructure to Draw trains, may change from containing value of a train object to a uptr of a train.

--- include/scene/Game.h
+++ include/scene/Game.h
@@ -1,11 +1,14 @@
 #ifndef GAME_H
 #define GAME_H
 
+#include "entity/Train.h"
 #include "utility/SpriteRenderer.h"
 
 #include <GLFW/glfw3.h>
 
 #include <memory>
+#include <vector>
+
 #include "ui/window.h"
 
 enum GameState
@@ -20,6 +23,8 @@ class Game
 private:
     GameState m_state;
     Game9::Window m_window;
+
+    std::vector<Game9::Train> m_trains;
 
 public:
     Game(std::shared_ptr<GLFWwindow> window, int width, int height);
--- src/scene/Game.cpp
+++ src/scene/Game.cpp
@@ -1,6 +1,7 @@
 #include "scene/Game.h"
 
 #include "glm/ext/matrix_clip_space.hpp"
+#include "glm/ext/vector_float2.hpp"
 
 #include <GL/gl.h>
 
@@ -9,7 +10,7 @@
 // Issue uses the same shader for background and train but still works.
 // Multiple SpriteRenderers for different things like background and entitys ?
 std::unique_ptr<SpriteRenderer> sceneRenderer;
-std::unique_ptr<SpriteRenderer> entityRenderer;
+std::shared_ptr<SpriteRenderer> entityRenderer;
 
 Game::Game(std::shared_ptr<GLFWwindow> window, int width, int height):
 m_state(GAME_ACTIVE),
@@ -50,14 +51,31 @@ void Game::Init()
     // Load Textures.
     ResourceManager::LoadTexture("resources/images/background.png", "background", 0);
     ResourceManager::LoadTexture("resources/images/train.png", "train", 1);
+
+    // Temp: in the future setting path may require to be copied from a file, and other way of adding trains to data structure.
+    m_trains.emplace_back(ResourceManager::GetTexture("train").value(), glm::vec2(m_window.GetWidth() / 2, m_window.GetHeight() /2), glm::vec2(64, 64), glm::vec2(50.0f, 50.0f));
+    m_trains.back().SetPath({glm::vec2(m_window.GetWidth() / 2, m_window.GetHeight() /2), glm::vec2{m_window.GetWidth(), m_window.GetHeight()}, glm::vec2{0,0}});
 }
 
 void Game::Render()
 {
     sceneRenderer->DrawSprite(ResourceManager::GetTexture("background").value(), glm::vec2(0.0f, 0.0f), glm::vec2(static_cast<float>(m_window.GetWidth()), static_cast<float>(m_window.GetHeight())));
-    entityRenderer->DrawSprite(ResourceManager::GetTexture("train").value(),
-                         glm::vec2(static_cast<float>(m_window.GetWidth()) / 2, static_cast<float>(m_window.GetHeight()) / 2),
-                         glm::vec2(256, 256));
+
+    for (auto& train: m_trains)
+    {
+        train.Draw(entityRenderer.get());
+    }
+}
+
+void Game::Update(float deltaTime)
+{
+    if (m_state == GAME_ACTIVE)
+    {
+        for(auto& train: m_trains)
+        {
+            train.Move(deltaTime);
+        }
+    }
 }
 
 void Game::ProcessInput(float deltaTime)
