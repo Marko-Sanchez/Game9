6a8de27ebc16e5c5f5339da7541edbc099654ee9
Marko-Sanchez <sanchezmarco0909@gmail.com>
2025-08-13 22:31:02 -0700

    Class to handle all train objects in game.

---
+++ include/entity/TrainHandler.h
@@ -0,0 +1,48 @@
+#ifndef TRAIN_HANDLER_H
+#define TRAIN_HANDLER_H
+
+#include <glm/glm.hpp>
+
+#include <array>
+#include <vector>
+#include <memory>
+#include <unordered_map>
+#include <string>
+#include <string_view>
+
+namespace Game9
+{
+
+/* All the possible train types currently available.*/
+enum class TrainTypes
+{
+    TRAM,
+    LIGHTRAIL,
+    SUBWAY,
+    COMMUTER,
+    FREIGHT
+};
+
+class TrainHandler
+{
+private:
+    /* File path to textures.*/
+    std::array<std::string_view, 2 /*TOTAL_TEXTURES*/> m_texturePaths;
+
+    /* Maps train names to its type.*/
+    std::unordered_map<std::string, TrainTypes> m_trainIdentifier;
+public:
+    TrainHandler();
+    ~TrainHandler();
+
+    void Draw();
+    void LoadPaths();
+    void AddPath();
+    /*
+    * AddPaths()? reads from file iostream >> '{1280, 1280}, {...}'
+    * -- file --
+    * train-type {...}
+    */
+};
+}// namespace Game9
+#endif
