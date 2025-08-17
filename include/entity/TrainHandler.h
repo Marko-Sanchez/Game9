0844c9863318b78022c326f0e1b95f77aebfb69a
Marko-Sanchez <sanchezmarco0909@gmail.com>
2025-08-16 20:33:20 -0700

    Added enum conversion for train types, instead of converting to integers converts to strings. Added two new function to add new trains and manage there paths. m_trainIdentifier now takes in a string_view instead of string.

--- include/entity/TrainHandler.h
+++ include/entity/TrainHandler.h
@@ -4,6 +4,8 @@
 #include <glm/glm.hpp>
 #include <glm/fwd.hpp>
 
+#include <nlohmann/json.hpp>
+
 #include "entity/Train.h"
 #include "utility/ResourceManager.h"
 #include "utility/SpriteRenderer.h"
@@ -12,7 +14,6 @@
 #include <vector>
 #include <memory>
 #include <unordered_map>
-#include <string>
 #include <string_view>
 
 namespace Game9
@@ -21,12 +22,24 @@ namespace Game9
 /* All the possible train types currently available.*/
 enum class TrainTypes
 {
+    UNDEFINED,
     TRAM,
     LIGHTRAIL,
     SUBWAY,
     COMMUTER,
     FREIGHT
 };
+
+// Specialized enum conversion, instead of converting enum to an integer value converts to string.
+NLOHMANN_JSON_SERIALIZE_ENUM(TrainTypes,
+                             {
+                             {TrainTypes::UNDEFINED, "undefined"},
+                             {TrainTypes::TRAM, "tram"},
+                             {TrainTypes::LIGHTRAIL, "lightrail"},
+                             {TrainTypes::SUBWAY, "subway"},
+                             {TrainTypes::COMMUTER, "commuter"},
+                             {TrainTypes::FREIGHT, "freight"},
+                             })
 
 class TrainHandler
 {
@@ -35,7 +48,7 @@ private:
     std::array<std::string_view, 2 /*TOTAL_TEXTURES*/> m_texturePaths;
 
     /* Maps train names to its type.*/
-    std::unordered_map<std::string, TrainTypes> m_trainIdentifier;
+    std::unordered_map<std::string_view, TrainTypes> m_trainIdentifier;
 
     /* Trains.*/
     std::vector<Train> m_trains;
@@ -46,7 +59,8 @@ public:
     void Draw(std::shared_ptr<SpriteRenderer> rend);
     void Update(float deltaTime);
     void LoadPaths(const std::string_view path);
-    void AddPath(const std::vector<glm::vec2> path);
+    void AddTrain(std::string_view name, std::string_view trainName, const std::vector<glm::vec2>& path);
+    void ExtendService(std::string_view trainName, const std::vector<glm::vec2>& path);
 };
 }// namespace Game9
 #endif
