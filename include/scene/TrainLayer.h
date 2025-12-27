96c188b3e064e62d229a07a437aae2e8b0b7311e
Marko-Sanchez <sanchezmarco0909@gmail.com>
2025-12-26 17:42:15 -0800

    Train layer to draw train objects above background layer or any other layer.

---
+++ include/scene/TrainLayer.h
@@ -0,0 +1,35 @@
+#ifndef TRAIN_LAYER_H
+#define TRAIN_LAYER_H
+
+#include "ui/Layer.h"
+#include "entity/TrainHandler.h"
+#include "utility/ResourceManager.h"
+#include "ui/window.h"
+
+#include <memory>
+
+namespace Game9
+{
+/*
+ * Draws trains together as a layer above background layer.
+ */
+class TrainLayer : public Core::Layer
+{
+private:
+    Core::util::ResourceManager m_resourceManager;
+
+    std::shared_ptr<Core::Window> m_window;
+
+    std::unique_ptr<Game9::TrainHandler> m_trainHandler;
+    std::shared_ptr<SpriteRenderer> m_entityRenderer;
+
+public:
+    TrainLayer(std::shared_ptr<Core::Window> window);
+
+    virtual ~TrainLayer() override;
+    virtual void OnRender() override;
+    virtual void OnUpdate(float delta) override;
+
+};
+}// namespace Game9
+#endif
---
+++ src/scene/TrainLayer.cpp
@@ -0,0 +1,43 @@
+#include "scene/TrainLayer.h"
+#include "glm/ext/matrix_clip_space.hpp"
+
+namespace Game9
+{
+TrainLayer::TrainLayer(std::shared_ptr<Core::Window> window):
+m_window(window)
+{
+    const std::string_view idName{"train"};
+
+    const std::string_view vertexShader{"resources/shaders/background.vertex"};
+    const std::string_view fragmentShader{"resources/shaders/background.fragment"};
+
+    glm::mat4 projection = glm::ortho(0.0f, static_cast<float>(m_window->GetWindowSpecification().width),
+                                      0.0f, static_cast<float>(m_window->GetWindowSpecification().height),
+                                      -1.0f, 1.0f);
+
+    m_resourceManager.LoadShader(vertexShader,fragmentShader, idName);
+    m_resourceManager.GetShader(idName)->Bind();
+    m_resourceManager.GetShader(idName)->SetUniform1i("u_image", 1);// Todo: hard-coded
+    m_resourceManager.GetShader(idName)->SetUniformMat4f("u_projection", projection);
+
+    // TrainHandler loads train textures and paths; EntityRenderer draws the trains.
+    m_trainHandler = std::make_unique<Game9::TrainHandler>();
+    m_entityRenderer = std::make_unique<SpriteRenderer>(m_resourceManager.GetShader("train"));
+
+    // loads all game assest.
+    m_trainHandler->LoadPaths();
+}
+
+TrainLayer::~TrainLayer()
+{}
+
+void TrainLayer::OnRender()
+{
+    m_trainHandler->Draw(m_entityRenderer);
+}
+
+void TrainLayer::OnUpdate(float delta)
+{
+    m_trainHandler->Update(delta);
+}
+}
