43b951fc487311f698380a2c7e405317bc25b559
Marko-Sanchez <sanchezmarco0909@gmail.com>
2026-01-01 19:48:43 -0800

    Event handling.

---
+++ include/ui/Event.h
@@ -0,0 +1,60 @@
+#ifndef EVENT_H
+#define EVENT_H
+
+#include <functional>
+
+namespace Core
+{
+// All current defined event classes.
+enum class EventType
+{
+    None = 0,
+    WindowClose, WindowResize,
+    KeyPressed, KeyReleased,
+    MouseButtonPressed, MouseButtonReleased, MouseMoved, MouseScrolled,
+};
+
+// Defines functions / contract from base class; inline, stringification.
+#define EVENT_CLASS_TYPE(type) static EventType GetStaticType() { return EventType::type; }\
+    virtual EventType GetEventType() const override { return GetStaticType(); }\
+    virtual const char* GetName() const override { return #type; }
+
+// Declares functions all event types must have.
+class Event
+{
+public:
+    bool isHandled = false;
+
+    virtual ~Event() {}
+    virtual EventType GetEventType() const = 0;
+    virtual const char* GetName() const = 0;
+};
+
+// Used within Layer::Event() to handle multiple events.
+class EventDispatcher
+{
+private:
+    Event &m_event;
+
+public:
+    EventDispatcher(Event& event):
+    m_event(event)
+    {}
+
+    /*
+     * @param
+     * func: lambda function that calls an events member function, to handle: mouse pressed event, etc.
+     */
+    template<typename T>
+    bool Dispatch(std::function<bool (T&)> func)
+    {
+        if (m_event.GetEventType() == T::GetStaticType() && !m_event.isHandled)
+        {
+            m_event.isHandled = func(*(T*)&m_event);
+            return true;
+        }
+        return false;
+    }
+};
+}// namespace Core
+#endif
