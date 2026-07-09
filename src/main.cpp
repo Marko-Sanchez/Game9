f802cb73c79ebd2d7e1a2409e38f522f3a0ab93e
Marko-Sanchez <sanchezmarco0909@gmail.com>
2026-07-09 12:43:19 -0700

    refactor: Organized files to more appropriate directories.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#include "core/Game.h"
+#include "game/Game.h"
 
 #include "layers/BackgroundLayer.h"
 #include "layers/TrainLayer.h"
@@ -8,7 +8,7 @@ int main()
     Core::ApplicationSpecification appspec{"Game9"};
     Core::Game application(appspec);
     application.PushLayer<Layer::BackgroundLayer>(application.GetWindow());
-    application.PushLayer<Layer::TrainLayer>(application.GetWindow());
+    // application.PushLayer<Layer::TrainLayer>(application.GetWindow());
     application.Run();
 
     return EXIT_SUCCESS;
