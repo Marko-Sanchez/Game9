a2a7f0ae79102b4bc122445bf74b5bbf59c77a0b
Marko-Sanchez <sanchezmarco0909@gmail.com>
2026-07-09 13:24:35 -0700

    feat: Base classes for objects in the world.

---
+++ src/core/compositecomponent.h
@@ -0,0 +1,62 @@
+#ifndef COMPOSITECOMPONENT_H
+#define COMPOSITECOMPONENT_H
+
+#include "world.h"
+
+#include <memory>
+#include <vector>
+
+namespace World
+{
+/*
+ * Base class for composites.
+ */
+class CompositeComponent: public WorldComponent
+{
+    protected:
+
+        glm::vec3 _origin{0.0f, 0.0f, 0.0f};
+        std::vector<std::shared_ptr<WorldComponent>> _children;
+
+    public:
+
+        void OnUpdate(float deltaSeconds) override
+        {
+            for (const auto& child: _children)
+            {
+                child->OnUpdate(deltaSeconds);
+            }
+        }
+
+        void OnRender() const override
+        {
+            for (const auto& child: _children)
+            {
+                child->OnRender();
+            }
+        }
+
+        void AddChildren(std::shared_ptr<WorldComponent> child) override
+        {
+            _children.push_back(std::move(child));
+        }
+
+        void RemoveChildren(Id childId) override
+        {
+            for (auto iter = _children.begin(); iter != _children.end(); ++iter)
+            {
+                if ((*iter)->GetId() == childId)
+                {
+                    _children.erase(iter);
+                }
+            }
+        }
+
+        glm::vec3 GetPosition() const override
+        {
+            return _origin;
+        }
+};
+}// World
+
+#endif
---
+++ src/core/world.h
@@ -0,0 +1,49 @@
+#ifndef WORLD_H
+#define WORLD_H
+
+#include <memory>
+#include <vector>
+
+#include <glm/glm.hpp>
+
+namespace World
+{
+/*
+ * Shared interface for anything that lives in the world. Design Pattern: Composite.
+ */
+class WorldComponent
+{
+    public:
+
+        using Id = std::uint64_t;
+
+        explicit WorldComponent(Id id, std::string name):
+            _id(id), _name(std::move(name)) {}
+
+        virtual ~WorldComponent() = default;
+
+        virtual void OnUpdate(float deltaSeconds = 0) {};
+        virtual void OnRender() const {};
+
+        virtual void AddChildren(std::shared_ptr<WorldComponent>) {};
+        virtual void RemoveChildren(Id) {};
+
+        virtual const std::vector<std::shared_ptr<WorldComponent>>& GetChildren() const
+        {
+            static const std::vector<std::shared_ptr<WorldComponent>> empty;
+            return empty;
+        };
+
+        virtual glm::vec3 GetPosition() const = 0;
+
+        Id GetId() const                   {return _id;}
+        const std::string& GetName() const {return _name;}
+
+    protected:
+
+        Id _id;
+        std::string _name;
+};
+}// namespace World
+
+#endif
